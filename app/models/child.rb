3ba86694ac77cb46590063e8480f65e0f35c786e
Yiding Liu <lyd@tamu.edu>
2016-03-28 21:57:18 +0000

    fix merge

--- app/models/child.rb
+++ app/models/child.rb
@@ -8,13 +8,3 @@ class Child < ActiveRecord::Base
         self.firstname+' '+self.lastname
     end
 end
-class Child < ActiveRecord::Base
-    belongs_to :parent
-    belongs_to :group
-    has_many :sent_invitations, class_name: "Invitation", as: :sender
-    has_many :received_invitations, class_name: "Invitation", as: :receiver
-    
-    def name
-        self.firstname+' '+self.lastname
-    end
-end
