7151731631ce445af56ffc3c5234b44acc37b28b
wangtjwork <wangpanda1993@tamu.edu>
2016-03-28 21:04:52 +0000

    xianshishi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+    attr_accessible :username , :password
+    
     has_one :parent
     has_one :mentor
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "parent" => "parent#index" 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
+  resources :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
