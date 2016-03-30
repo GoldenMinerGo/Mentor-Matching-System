c0ab3799f214cd6fcc98bbc3405722e7f238219a
rtzzzzz <zrk.strike@outlook.com>
2016-03-30 01:55:36 +0000

    group delete

--- app/controllers/group_controller.rb
+++ app/controllers/group_controller.rb
@@ -1,14 +1,15 @@
 class GroupController < ApplicationController
     def index
-        user = User.find(1)
+        user = User.find(4)
         @parent = user.parent
         @groups = Group.where(:admin_id => @parent)[0]
+        #@groups = Group.all
         #@members = @groups.members
     end
     
     def edit
         #group 
-        user = User.find(1)
+        user = User.find(4)
         @parent = user.parent
         #@children=@parent.children
         #if you use where, @groups is an array, like [group1,group2],then
@@ -17,7 +18,7 @@ class GroupController < ApplicationController
     end
     
     def update
-        user=User.find(1)
+        user=User.find(4)
         @parent=user.parent
         @groups=Group.where(:admin_id => @parent)[0]
         if @groups.update_attributes!(group_params)
@@ -28,11 +29,11 @@ class GroupController < ApplicationController
     end
     
     def destroy
-        user=User.find(1)
+        user=User.find(4)
         @parent=user.parent
         @groups=Group.where(:admin_id => @parent)[0]
         @groups.destroy
-        flash[:notice] = "Your group '#{@groups.title}' closed."
+        #flash[:notice] = "Your group '#{@groups.title}' closed."
         redirect_to group_path
     end
     
