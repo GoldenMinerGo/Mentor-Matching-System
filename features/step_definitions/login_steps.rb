5c72e3702a41514093b381ac4fd2523cc6bd8e5e
fanloyu <yifanjiang@hotmail.com>
2016-04-01 02:56:02 +0000

    fix multiple features test

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,8 +1,3 @@
-Given(/^the following users exist:$/) do |users_table|
-  users_table.hashes.each do |user|
-    User.create!(user)
-end
-end
 
 Then(/^I will nullify my session hash$/) do
   session[:user_id] = nil
